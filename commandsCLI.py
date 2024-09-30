a96ceb2c7ed3641371804200e03142b34e2e2793
luisca12 <solera.luis12@gmail.com>
2024-09-30 14:25:47 -0400

    Added interface check

--- commandsCLI.py
+++ commandsCLI.py
@@ -2,10 +2,13 @@ from netmiko import ConnectHandler
 from log import authLog
 
 import traceback
+import csv
 import os
+import re
 
 shHostname = "show run | i hostname"
-shRunDevice = "show run | sec device-sensor|device-tracking"
+shIntStatus = "show interface status"
+shRunDevice = "show run | sec device-sensor"
 
 shVlanID20 = "show vlan id 20"
 
@@ -36,11 +39,14 @@ deviceTrackingConf = [
 
 # Regex Patterns
 intPatt = r'[a-zA-Z]+\d+\/(?:\d+\/)*\d+'
+shIntDevTrack = re.compile(r'(device-tracking attach-policy DEVTRK)|(device-tracking attach-policy IPDT_POLICY)')
 
 def complCheck(validIPs, username, netDevice):
-    # This function is to take a show run
+    # This function is to check for compliance
 
     for validDeviceIP in validIPs:
+        missingConfig1 = False
+        deviceConfigured = False
         configInDevice = []
         missingConfig = []
         try:
@@ -58,9 +64,11 @@ def complCheck(validIPs, username, netDevice):
                 'session_log_file_mode': 'append'
             }
 
-            print(f"Connecting to device {validDeviceIP}...")
+            print(f"INFO: Connecting to device {validDeviceIP}...")
+            authLog.info(f"Connecting to device {validDeviceIP}...")
             with ConnectHandler(**currentNetDevice) as sshAccess:
                 try:
+                    authLog.info(f"Connected to device {validDeviceIP}")
                     sshAccess.enable()
                     shHostnameOut = sshAccess.send_command(shHostname)
                     authLog.info(f"User {username} successfully found the hostname {shHostnameOut}")
@@ -80,22 +88,64 @@ def complCheck(validIPs, username, netDevice):
                         authLog.info(f"Automation successfully ran the command:{shRunDevice}\n{shHostnameOut}{shRunDevice}\n{shRunDeviceOut}")
 
                         for index, item in enumerate(deviceTrackingConf):
+                            print(f"INFO: Checking for \"{item}\" in {validDeviceIP}")
                             if not item in shRunDeviceOut:
-                                missingConfig.append(item)
+                                # missingConfig.append(item)
                                 authLog.info(f"Configuration: {item} is missing from device {validDeviceIP}")
+                                authLog.info(f"Skipping device: {validDeviceIP}")
+                                print(f"INFO: Configuration: {item} is missing from device {validDeviceIP}, skipping device")
+                                missingConfig1 = True
+                                break
                             else:
-                                configInDevice.append(item)
+                                # configInDevice.append(item)
+                                missingConfig1 = False
                                 authLog.info(f"Configuration: {item} was found on device {validDeviceIP}")
+
+                        if missingConfig1 == True:
+                            with open('missingDeviceTrack_Configuration.csv', mode='a', newline='') as file:
+                                writer = csv.writer(file)
+                                writer.writerow([validDeviceIP])
+                            continue
+
+                        shIntStatusOut = sshAccess.send_command(shIntStatus)
+                        authLog.info(f"Automation ran the command \"{shIntStatus}\" on device {validDeviceIP}\n{shHostnameOut}{shIntStatusOut}")
+                        print(f"INFO: Running the following command: \"{shIntStatus}\" on device {validDeviceIP}\n{shHostnameOut}{shIntStatusOut}")
+                        shIntStatusOut1 = re.findall(intPatt, shIntStatusOut)
+                        authLog.info(f"Automation found the following interfaces on device {validDeviceIP}: {shIntStatusOut1}")
+
+                        for interface in shIntStatusOut1:
+                            interfaceOut = sshAccess.send_command(f'show run int {interface}')
+                            if not shIntDevTrack.search(interfaceOut):
+                                authLog.info(f"Configuration: {shIntDevTrack.pattern} is missing from device: {validDeviceIP} on interface {interface}")
+                                authLog.info(f"Skipping device {validDeviceIP}")
+                                print(f"INFO: Skipping device {validDeviceIP}")
+                                missingConfig1 = True
+                                break
+                            else:
+                                print(f"INFO: Interface {interface} has configured {shIntDevTrack.pattern} on device {validDeviceIP}")
+                                authLog.info(f"Interface {interface} has configured {shIntDevTrack.pattern} on device {validDeviceIP}")
+                                missingConfig1 = False
+                                deviceConfigured = True
+
+                        if deviceConfigured == True:
+                            with open('configuredDevices_DeviceTrack.csv', mode='a', newline='') as file:
+                                writer = csv.writer(file)
+                                writer.writerow([validDeviceIP])
                 
-                        with open(f"Outputs/{validDeviceIP}_complianceCheck-DevTrack.txt", "a") as file:
-                            file.write(f"User {username} connected to device IP {validDeviceIP}\n\n")
-                            file.write("="*20 + "\n")
-                            file.write(f"Below is the missing configuration:\n")
-                            file.write(f"{shHostnameOut}\n{'\n'.join(missingConfig)}\n\n")
-                            file.write("="*20 + "\n")
-                            file.write(f"Below is the current configuration:\n")
-                            file.write(f"{shHostnameOut}{shRunDevice}\n{'\n'.join(configInDevice)}\n\n")
-                            authLog.info(f"File {validDeviceIP}_dhcpSnoopCheck.txt was created successfully.")
+                        # with open(f"Outputs/{validDeviceIP}_complianceCheck-DevTrack.txt", "a") as file:
+                        #     file.write(f"User {username} connected to device IP {validDeviceIP}\n\n")
+                        #     file.write("="*20 + "\n")
+                        #     file.write(f"Below is the missing configuration:\n")
+                        #     file.write(f"{shHostnameOut}\n{'\n'.join(missingConfig)}\n\n")
+                        #     file.write("="*20 + "\n")
+                        #     file.write(f"Below is the current configuration:\n")
+                        #     file.write(f"{shHostnameOut}{shRunDevice}\n{'\n'.join(configInDevice)}\n\n")
+                        #     authLog.info(f"File {validDeviceIP}_dhcpSnoopCheck.txt was created successfully.")
+
+                        print(f"Outputs and files successfully created for device {validDeviceIP}.\n")
+                        print("For any erros or logs please check Logs -> authLog.txt\n")
+                        print(f"Program finished, all the configuration has been applied.")
+                        
                     else:
                         print(f"INFO: Device: {validDeviceIP}, is a Caremore Site device")
                         authLog.info(f"Device: {validDeviceIP}, is a Caremore Site device")   
@@ -113,8 +163,3 @@ def complCheck(validIPs, username, netDevice):
             authLog.error(traceback.format_exc(),"\n")
             with open(f"failedDevices.txt","a") as failedDevices:
                 failedDevices.write(f"User {username} connected to {validDeviceIP} got an error.\n")
-        
-        finally:
-            print(f"Outputs and files successfully created for device {validDeviceIP}.\n")
-            print("For any erros or logs please check Logs -> authLog.txt\n")
-            print(f"Program finished, all the configuration has been applied.")
